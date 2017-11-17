504ac690f4b2141583dd66de986f2e28b95015e1
XPS13-9360\Gregor Fernbach <gregor.fernbach.2@mailbox.org>
2017-11-17 12:15:38 +0100

    Github username to github.csv file

--- labs/src/main/scala/at/fhj/swengb/apps/helloworld/hello.scala
+++ labs/src/main/scala/at/fhj/swengb/apps/helloworld/hello.scala
@@ -2,6 +2,6 @@ package at.fhj.swengb.apps.helloworld
 
 object hello {
   def main(args: Array[String]): Unit = {
-    println("Hello Rest der Klasse keine Ahnung, Qysh jeni a jeni mir?")
+    println("Hello Rest der Klasse keine Ahnung, Qysh jeni a jeni mir?, Gregor was here")
   }
 }

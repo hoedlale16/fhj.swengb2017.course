3993123f61e708bb63b3f44c688a55d31c6fe25e
Alexander Hoedl <alexander.hoedl@edu.fh-joanneum.at>
2017-11-17 12:35:13 +0100

    cleanup: fixed merge compile errors

--- labs/src/main/scala/at/fhj/swengb/apps/helloworld/Hello.scala
+++ labs/src/main/scala/at/fhj/swengb/apps/helloworld/Hello.scala
@@ -1,6 +1,5 @@
 package at.fhj.swengb.apps.helloworld
 
-
 object Hello extends Greeting with App {
   println(greeting)
 }
--- labs/src/main/scala/at/fhj/swengb/apps/helloworld/hello.scala
+++
@@ -1,7 +0,0 @@
-package at.fhj.swengb.apps.helloworld
-
-object hello {
-  def main(args: Array[String]): Unit = {
-    println("Hello Rest der Klasse keine Ahnung, Qysh jeni a jeni mir?, Gregor was here. Alex too")
-  }
-}

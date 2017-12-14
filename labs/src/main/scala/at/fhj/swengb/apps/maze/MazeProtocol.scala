755e594f3c82c038f6e4fba15fa456a915a25f9a
Alexander Hoedl <alexander.hoedl@edu.fh-joanneum.at>
2017-12-14 10:55:38 +0100

    implementation course

--- labs/src/main/scala/at/fhj/swengb/apps/maze/MazeProtocol.scala
+++ labs/src/main/scala/at/fhj/swengb/apps/maze/MazeProtocol.scala
@@ -9,7 +9,42 @@ import scala.collection.JavaConverters._
   */
 object MazeProtocol {
 
-  def convert(pos: MazeProtobuf.Pos): Pos = ???
+  def convert(pos: MazeProtobuf.Pos): Pos = Pos(pos.getX, pos.getY)
+
+  def convert(pbCell: MazeProtobuf.Cell): Cell = {
+    val pbPos = pbCell.getPos
+    val pbTopLeft = pbCell.getTopLeft
+    val pbRegion = pbCell.getRegion
+
+    val up: Option[Pos] =
+      if (pbCell.getNoneUp)
+        None
+      else
+        Some(Pos(pbCell.getUp.getX, pbCell.getUp.getY))
+
+    val right: Option[Pos] = pbCell.getNoneRight match {
+      case true  => None
+      case false => Option(Pos(pbCell.getRight.getX, pbCell.getRight.getY))
+    }
+
+    val down: Option[Pos] = pbCell.getNoneDown match {
+      case true  => None
+      case false => Option(Pos(pbCell.getDown.getX, pbCell.getDown.getY))
+    }
+
+    val left: Option[Pos] = pbCell.getNoneLeft match {
+      case true  => None
+      case false => Option(Pos(pbCell.getLeft.getX, pbCell.getLeft.getY))
+    }
+
+    Cell(Pos(pbPos.getX, pbPos.getY),
+         Coord(pbTopLeft.getX, pbTopLeft.getY),
+         Rect(pbRegion.getWidth, pbRegion.getHeight),
+         up,
+         right,
+         down,
+         left)
+  }
 
   def convert(end: Pos): MazeProtobuf.Pos =
     MazeProtobuf.Pos.newBuilder().setX(end.x).setY(end.y).build()
@@ -21,26 +56,60 @@ object MazeProtocol {
       .setWidth(cellRect.width)
       .build()
 
-  def convert(cell: Cell): MazeProtobuf.Cell =
-    MazeProtobuf.Cell
+  def convert(cell: Cell): MazeProtobuf.Cell = {
+    val pbCell = MazeProtobuf.Cell
       .newBuilder()
-      .build
+      .setPos(MazeProtobuf.Pos.newBuilder().setX(cell.pos.x).setY(cell.pos.y))
+      .setTopLeft(MazeProtobuf.Coord.newBuilder().setX(cell.x).setY(cell.y))
+      .setRegion(
+        MazeProtobuf.Rect
+          .newBuilder()
+          .setWidth(cell.region.width)
+          .setHeight(cell.region.height))
+
+    cell.up match {
+      case Some(up) => pbCell.setUp(convert(up))
+      case None     => pbCell.setNoneUp(true)
+
+    }
+
+    cell.right match {
+      case Some(right) => pbCell.setUp(convert(right))
+      case None        => pbCell.setNoneRight(true)
+    }
+
+    cell.down match {
+      case Some(down) => pbCell.setUp(convert(down))
+      case None       => pbCell.setNoneDown(true)
+    }
+
+    cell.left match {
+      case Some(left) => pbCell.setUp(convert(left))
+      case None       => pbCell.setNoneLeft(true)
+    }
+
+    //Return cell
+    pbCell.build()
+
+  }
 
   /**
     * Provided a protobuf encoded maze, create a business model class 'maze' again
     */
-  // TODO implement missing functionality
   def convert(protoMaze: MazeProtobuf.Maze): Maze = {
-    println(protoMaze.getSizeX)
-    println(protoMaze.getSizeY)
+
+    //Create Maze-Grid
+    val mazeGrid: Array[Cell] =
+      protoMaze.getGridList.asScala.map(e => convert(e)).toArray[Cell]
+
+    //Create Maze
     Maze(
       protoMaze.getSizeX,
       protoMaze.getSizeY,
-      Pos(0, 0), // TODO read from proto
-      Pos(1, 0), // TODO read from proto
-      Array.fill(protoMaze.getSizeX * protoMaze.getSizeY)(
-        Cell(Pos(0, 0), Coord(0, 0), Rect(0, 0))), // TODO read from proto
-      Rect(100, 100) // TODO read from proto
+      Pos(protoMaze.getStart.getX, protoMaze.getStart.getY),
+      Pos(protoMaze.getEnd.getX, protoMaze.getEnd.getY),
+      mazeGrid,
+      Rect(protoMaze.getCellRect.getWidth, protoMaze.getCellRect.getHeight)
     )
   }
 
--- labs/src/test/scala/at/fhj/swengb/apps/maze/MazeMarshallerSpec.scala
+++ labs/src/test/scala/at/fhj/swengb/apps/maze/MazeMarshallerSpec.scala
@@ -2,7 +2,6 @@ package at.fhj.swengb.apps.maze
 
 import org.scalatest.WordSpecLike
 
-
 /**
   * Specification for Maze marshaller.
   */
@@ -24,5 +23,29 @@ class MazeMarshallerSpec extends WordSpecLike {
       assert(pos.width == actual.getWidth)
       assert(pos.height == actual.getHeight)
     }
+
+  }
+  ".conect(cell: Cell) - None options" in {
+    val pos = Pos(4711, 815)
+    val coord = Coord(2, 2)
+    val rect = Rect(5, 5)
+
+    val cell = Cell(pos, coord, rect, None,None,None,None)
+    val actual: MazeProtobuf.Cell = MazeProtocol.convert(cell)
+
+    //Some crazy tests are going on...
+    assert(cell == MazeProtocol.convert(actual))
+  }
+
+  ".conect(cell: Cell) - Some options" in {
+    val pos = Pos(4711, 815)
+    val coord = Coord(2, 2)
+    val rect = Rect(5, 5)
+
+    val cell = Cell(pos, coord, rect, Some(pos),None,None,None)
+    val actual: MazeProtobuf.Cell = MazeProtocol.convert(cell)
+
+    //Some crazy tests are going on...
+    assert(cell == MazeProtocol.convert(actual))
   }
 }
